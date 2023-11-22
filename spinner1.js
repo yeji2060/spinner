d0e4362fce1e5b68906f91162dd9b5ccc75e3059
Yeji Ha <yeji206000@gmail.com>
2023-11-22 12:08:39 -0800

    edited

--- spinner1.js
+++ spinner1.js
@@ -16,3 +16,15 @@ setTimeout(() => {
   // Need to escape the backslash since it's a special character.
   process.stdout.write('\r\\   '); 
 }, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 1300);
